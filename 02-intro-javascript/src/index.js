45285eb0d85f44fb59d719d2d9bd7a6682c952e4
MartinAlejandroDiaz <diazmartin_95@yahoo.com.ar>
2020-09-11 01:04:24 -0300

    start-react-courses

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,13 @@
+// Variables y Constantes
+
+const nombre = 'tino';
+const apellido = 'diaz';
+
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado);
+if (true){
+    let valordado = 6;
+    console.log(valordado);
+}
+console.log(valorDado);
